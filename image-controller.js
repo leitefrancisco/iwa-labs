0000c7feb332458df6a4802f1561189b1dbecd23
leitefrancisco <franciscolmf@outlook.com>
2022-04-08 09:03:22 +0000

    functions get and add

--- image-controller.js
+++ image-controller.js
@@ -3,4 +3,33 @@ const UPLOAD_PATH = require('./routes').UPLOAD_PATH,
       path = require('path'), 
       fs = require('fs'), 
       del = require('del');
-7
+
+exports.uploadImage = function(req, res) { 
+    let newImage = new Image(); 
+    newImage.filename = req.file.filename; 
+    newImage.originalName = req.file.originalname; 
+    newImage.desc = req.body.desc; 
+    newImage.save(err => { 
+        if (err) { 
+            return res.sendStatus(400); 
+        } 
+        res.status(201).send({ newImage }) 
+    }); 
+};
+
+exports.getImages = function(req, res) { 
+    Image.find({}, '-__v') 
+    .lean() 
+    .exec((err, images) => { 
+        if (err) { 
+            return res.sendStatus(400); 
+        } 
+ 
+        for (let i = 0; i < images.length; i++) { 
+            var img = images[i]; 
+            img.url = req.protocol + '://' + req.get('host') + '/images/' + img._id; 
+        } 
+ 
+        res.json(images); 
+    }); 
+};
