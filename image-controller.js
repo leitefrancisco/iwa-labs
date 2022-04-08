704a86995007b658092f901fe78a918212c8da62
leitefrancisco <franciscolmf@outlook.com>
2022-04-08 08:36:48 +0000

    img controller

---
+++ image-controller.js
@@ -0,0 +1,6 @@
+const UPLOAD_PATH = require('./routes').UPLOAD_PATH, 
+      Image = require('./models/image'), 
+      path = require('path'), 
+      fs = require('fs'), 
+      del = require('del');
+7
---
+++ models/image.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose'); 
+ 
+const imageSchema = new mongoose.Schema({ 
+    filename: String, 
+    originalName: String, 
+    desc: String, 
+    created: Date 
+}); 
+ 
+module.exports = mongoose.model('Image', imageSchema);
+
