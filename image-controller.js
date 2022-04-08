931e624d4966dfafbf3e1fd8a4119b495f209de0
leitefrancisco <franciscolmf@outlook.com>
2022-04-08 09:05:02 +0000

    functions to images

--- image-controller.js
+++ image-controller.js
@@ -24,12 +24,38 @@ exports.getImages = function(req, res) {
         if (err) { 
             return res.sendStatus(400); 
         } 
- 
         for (let i = 0; i < images.length; i++) { 
             var img = images[i]; 
             img.url = req.protocol + '://' + req.get('host') + '/images/' + img._id; 
         } 
  
         res.json(images); 
+    }); 
+};
+
+exports.getImage = function(req, res) { 
+    let imgId = req.params.id; 
+ 
+    Image.findById(imgId, (err, image) => { 
+        if (err) { 
+            return res.sendStatus(400); 
+        } 
+ 
+        res.setHeader('Content-Type', 'image/jpeg'); 
+        fs.createReadStream(path.join(UPLOAD_PATH, image.filename)).pipe(res); 
+    }); 
+};
+
+exports.deleteImage = function(req, res) { 
+    let imgId = req.params.id; 
+ 
+    Image.findByIdAndRemove(imgId, (err, image) => { 
+        if (err && image) { 
+            return res.sendStatus(400); 
+        } 
+ 
+        del([path.join(UPLOAD_PATH, image.filename)]).then(deleted => { 
+            res.sendStatus(200); 
+        }); 
     }); 
 };
